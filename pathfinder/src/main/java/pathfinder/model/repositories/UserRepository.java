9be79e0d5660bb38eda279c14eb64519b78cb6e5
laszlokiss <laszlo.kiss9203@gmail.com>
2016-10-17 17:53:17 +0200

    spring boot init

--- pathfinder/src/main/java/pathfinder/model/repositories/UserRepository.java
+++ pathfinder/src/main/java/pathfinder/model/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package pathfinder.model.repositories;
 
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.stereotype.Repository;
 
-import pathfinder.model.entities.User;
+import pathfinder.model.nodes.User;
 
+@Repository
 public interface UserRepository extends GraphRepository<User> {
 
 	
