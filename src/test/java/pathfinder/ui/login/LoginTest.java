08d8bc637ad56176a4eee0a1f33a54fc3019a36a
Széles Adorján <szeles.adorjan@hotmail.com>
2016-10-29 18:08:10 +0200

    Teszt mappák hozzáadása. Travis teszt futtatás kiegészítése.

---
+++ src/test/java/pathfinder/ui/login/LoginTest.java
@@ -0,0 +1,18 @@
+package pathfinder.ui.login;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A bejelentkező képernyő teszt osztálya.
+ *
+ * @author Széles Adorján
+ * Date: 2016. 10. 29.
+ */
+public class LoginTest {
+
+    @Test
+    public void testLoginSuccessfully() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
